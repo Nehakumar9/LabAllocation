4be3a5c119e0e87bbf18f71137a6006b3af2714e
Nehakumar9 <nehakumar0901@gmail.com>
2023-07-08 18:03:34 +0530

    Lab Allocation commited

---
+++ src/laballocation/LabAllocation.java
@@ -0,0 +1,30 @@
+package laballocation;
+
+import java.util.Scanner;
+
+public class LabAllocation {
+
+	public static void main(String[] args) {
+		 Scanner scanner=new Scanner(System.in);
+			int x=scanner.nextInt();
+			int y=scanner.nextInt();
+			int z=scanner.nextInt();
+			int n=scanner.nextInt();
+			while(true){
+			    n+=1;
+			    if(n==x){
+			        System.out.println("l1");
+			        break;
+			    }
+			    if(n==y){
+			        System.out.println("l2");
+			        break;
+			    }
+			    if(n==z) {
+					System.out.println("l3");
+					break;
+				}
+			}
+			scanner.close();
+	}
+}
